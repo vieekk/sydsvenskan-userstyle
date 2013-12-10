72032eb330690dec35b7fb3a691eba183c2eea84
vieekk <vieekk@gmail.com>
2013-12-10 13:57:49 +0100

    Some renaming

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 version=`cat version`
-rm bin/css/*.min.css
-java -jar lib/yuicompressor.jar src/css/sydsvenskan.user.css -o bin/css/sydsvenskan.user.min.css
-cp bin/css/sydsvenskan.user.min.css "bin/css/sydsvenskan-$version.user.min.css"
+rm bin/css/*.css
+java -jar lib/yuicompressor.jar src/css/sydsvenskan.user.css -o bin/css/sydsvenskan.user.css
+cp bin/css/sydsvenskan.user.css "bin/css/sydsvenskan-$version.user.css"
