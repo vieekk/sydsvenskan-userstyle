7faeab9874271a3a76f82a267c003b17355b46ca
vieekk <vieekk@gmail.com>
2013-12-13 08:52:30 +0100

    Sans-serif for body text

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 version=`cat version`
-rm bin/*
+rm bin/* -r
 java -jar lib/yuicompressor.jar src/sydsvenskan.user.css -o bin/sydsvenskan.user.css
 cp bin/sydsvenskan.user.css "bin/sydsvenskan-$version.user.css"
