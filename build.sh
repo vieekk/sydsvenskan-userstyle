c54a88e7d35f41bf82e79cc09c77eb7d95344d5d
vieekk <vieekk@gmail.com>
2013-12-14 11:49:36 +0100

    Commented build script

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-rm -f bin/sydsvenskan-*
+# clean bin dir
+rm -rf bin/*
 
 cd src
+# concatenate and minify
 cat head.css sydsvenskan.css feet.css > cat.css
 java -jar ../lib/yuicompressor.jar cat.css -o ../bin/sydsvenskan.min.css
 rm cat.css
+cd ..
 
-cd ../bin
+cd bin
+# read and save version
 version=`cat ../version`
 echo "Current version: v$version"
 echo -n "    New version: v"
 read version
 echo "$version" > ../version
-
 echo "/* v$version https://github.com/vieekk/sydsvenskan-userstyle */ " > version.css
 cat version.css sydsvenskan.min.css > "sydsvenskan-$version.min.css"
 cp "sydsvenskan-$version.min.css" sydsvenskan.min.css 
