febff31530c26ed0a54ac2e638fe35f666f577bb
vieekk <vieekk@gmail.com>
2013-12-14 12:39:19 +0100

    Display version

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ echo "Current version: v$version"
 echo -n "    New version: v"
 read version
 echo "$version" > ../version
-echo "/* v$version https://github.com/vieekk/sydsvenskan-userstyle */ " > version.css
-cat version.css sydsvenskan.min.css > "sydsvenskan-$version.min.css"
-cp "sydsvenskan-$version.min.css" sydsvenskan.min.css 
-rm version.css
+cat ../src/version.css sydsvenskan.min.css > "sydsvenskan-$version.min.css"
+# replace version placeholders
+sed 's/\$version/'"$version"'/g' "sydsvenskan-$version.min.css" > sydsvenskan.min.css
+cp sydsvenskan.min.css "sydsvenskan-$version.min.css"
 echo " . . . . . . . . Done!"
